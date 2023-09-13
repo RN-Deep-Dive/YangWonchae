659af0c69da364c55a2fcca660a4b5e08638edde
ywonchae1 <ywonchae62@gmail.com>
2023-09-14 07:38:48 +0900

    카카오톡 친구 목록 클론코딩: styled component

--- part2-ch1-kakao-friend-list/src/Profile.js
+++ part2-ch1-kakao-friend-list/src/Profile.js
@@ -1,21 +1,43 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
 import Margin from './Margin'
+import styled from 'styled-components'
+
+const Container = styled.View`
+  flex-direction: row;
+`
+const ProfileImage = styled.Image`
+  width: ${props => (props.size)};
+  height: ${props => (props.size)};
+  borderRadius: ${props => (props.size * 0.4)}px;
+`
+const TextContainer = styled.View`
+  justifyContent: center;
+  marginLeft: 10
+`
+const NameText = styled.Text`
+  fontWeight: ${props => (props.isMe ? 'bold' : 'normal')};
+  fontSize: ${props => (props.isMe ? 16 : 15)}px;
+`
+const IntroductionText = styled.Text`
+  fontSize: ${props => (props.isMe ? 12 : 11)}px;
+  color: grey;
+`
 
 export default ({uri, name, introduction, isMe}) => {
   const size = isMe ? 50 : 40
   return (
-    <View style={{flexDirection: 'row'}}>
-      <Image source={{uri}} style={{width: size, height: size, borderRadius: size * 0.4}} />
-      <View style={{justifyContent: 'center', marginLeft: 10}}>
-        <Text style={{fontWeight: isMe ? 'bold' : undefined, fontSize: isMe ? 16 : 15}}>{name}</Text>
+    <Container>
+      <ProfileImage source={{uri}} size={size} />
+      <TextContainer>
+        <NameText isMe={isMe}>{name}</NameText>
         {!!introduction && (
           <View>
             <Margin height={isMe ? 6 : 2} />
-            <Text style={{fontSize: isMe ? 12 : 11, color: 'grey'}}>{introduction}</Text>
+            <IntroductionText isMe={isMe}>{introduction}</IntroductionText>
           </View>
         )}
-      </View>
-    </View>
+      </TextContainer>
+      </Container>
   )
 }
