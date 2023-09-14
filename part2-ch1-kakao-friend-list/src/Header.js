1b59e226191c5fe0f985997dbc2dcfd3f9dcb213
ywonchae1 <ywonchae62@gmail.com>
2023-09-14 11:13:46 +0900

    +화면 따라 헤더 아이콘 표시 다르게

--- part2-ch1-kakao-friend-list/src/Header.js
+++ part2-ch1-kakao-friend-list/src/Header.js
@@ -2,23 +2,24 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Ionicons } from '@expo/vector-icons'
 
-const IconButton = ({name, bgColor}) => {
+const IconButton = ({name}) => {
     return (
-        <TouchableOpacity hitSlop={{top: 10, bottom: 10}} style={{paddingHorizontal: 6, backgroundColor: bgColor}}>
+        <TouchableOpacity hitSlop={{top: 10, bottom: 10}} style={{paddingHorizontal: 6}}>
             <Ionicons name={name} size={24} color="black" />
         </TouchableOpacity>
     )
 }
 
-export default () => {
+export default ({title}) => {
   return (
-    <View style={{flexDirection: 'row', justifyContent: 'space-between', paddingVertical: 10}}>
-      <Text style={styles.title}>친구</Text>
+    <View style={{flexDirection: 'row', justifyContent: 'space-between', paddingVertical: 10, backgroundColor: 'white'}}>
+      <Text style={styles.title}>{title}</Text>
 
       <View style={{flexDirection: 'row'}}>
         <IconButton name="search-outline" />
-        <IconButton name="person-add-outline" />
-        <IconButton name="md-musical-notes-outline" />
+        {title === '친구' && <IconButton name="person-add-outline" />}
+        {title === '친구' && <IconButton name="md-musical-notes-outline" />}
+        {title === '채팅' && <IconButton name="ios-chatbubbles-outline" />}
         <IconButton name="ios-settings-outline" />
       </View>
     </View>
